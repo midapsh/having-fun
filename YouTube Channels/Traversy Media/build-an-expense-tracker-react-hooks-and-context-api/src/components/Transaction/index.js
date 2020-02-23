a3de944bd09ed12d1bd87f5093859607edf9a747
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-23 00:21:49 -0300

    Remove key from component Transaction

--- YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/Transaction/index.js
+++ YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/Transaction/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { formatCurrency } from "../../misc/CurrencyFormat";
 
-export default function Transaction({ key, text, amount }) {
+export default function Transaction({ text, amount }) {
     return (
-        <li key={key} className={Math.sign(amount) === 1 ? "plus" : "minus"}>
+        <li className={Math.sign(amount) === 1 ? "plus" : "minus"}>
             {text} <span>{formatCurrency("pt-BR", "BRL", amount.toFixed(2).toString())}</span><button className="delete-btn">x</button>
         </li>
     )
