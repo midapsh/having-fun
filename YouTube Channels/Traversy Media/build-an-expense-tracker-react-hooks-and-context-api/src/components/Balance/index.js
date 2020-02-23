565af10b476090b106f84060343cc93f0e57f431
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-23 00:21:14 -0300

    Customize balance values; use real values

--- YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/Balance/index.js
+++ YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/Balance/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { GlobalContext } from "../../context/GlobalState";
+import { formatCurrency } from "../../misc/CurrencyFormat";
 
 export default function Balance() {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0);
+
     return (
         <>
             <h4>Your Balance</h4>
-            <h1>R$0,00</h1>
+            <h1>{formatCurrency("pt-BR", "BRL", total.toFixed(2).toString())}</h1>
         </>
     )
 }
