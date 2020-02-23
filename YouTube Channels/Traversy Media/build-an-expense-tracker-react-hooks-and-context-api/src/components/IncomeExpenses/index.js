914700750de8c61509026cbb4d65fb5a82aab570
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-23 00:21:27 -0300

    Customize IncomeExpenses values; use real values

--- YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/IncomeExpenses/index.js
+++ YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/components/IncomeExpenses/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { GlobalContext } from "../../context/GlobalState";
+import { formatCurrency } from "../../misc/CurrencyFormat";
 
 export default function IncomeExpenses() {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts
+        .filter(item => Math.sign(item) === 1)
+        .reduce((acc, item) => (acc += item), 0);
+
+    const expense = amounts
+        .filter(item => Math.sign(item) === -1)
+        .reduce((acc, item) => (acc -= item), 0);
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+R$0,00</p>
+                <p className="money plus">{formatCurrency("pt-BR", "BRL", income.toFixed(2).toString())}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-R$0,00</p>
+                <p className="money minus">{formatCurrency("pt-BR", "BRL", expense.toFixed(2).toString())}</p>
             </div>
         </div>
     )
