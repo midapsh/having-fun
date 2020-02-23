cfcfbea0d93920c2d48a61d55ee72fe13104daf6
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-22 22:41:48 -0300

    Create Balance and IncomeExpenses components

--- YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/App.js
+++ YouTube Channels/Traversy Media/build-an-expense-tracker-react-hooks-and-context-api/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
-      Hello =)
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </>
   );
 }
